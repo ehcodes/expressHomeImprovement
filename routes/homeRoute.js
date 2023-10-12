1d81de7779294a2601b4e20061e3bb4e82b81267
ehcodes <engrid.hernandez@gmail.com>
2023-10-12 08:53:49 -0400

    added res.send

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -3,6 +3,9 @@ const homes = require("../controllers/homesController");
 const homeSeed = require("../homeSeed")
 
 router.get("/", homes.getAllHomes);
+router.get("/", async (req, res) => {
+  res.send('Welcome, <a href="/homes/seed">Seed db</a>');
+});
 
 router.get('/homes/seed', (req, res)=>{
   Home.create(homeSeed, (err, data)=>{
