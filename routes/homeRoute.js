dfb76fa4abff2e56050f30530f3faa2f0e4547ae
ehcodes <engrid.hernandez@gmail.com>
2023-10-12 08:56:59 -0400

    updated route for getting all homes

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const homes = require("../controllers/homesController");
 const homeSeed = require("../homeSeed")
 
-router.get("/", homes.getAllHomes);
 router.get("/", async (req, res) => {
   res.send('Welcome, <a href="/homes/seed">Seed db</a>');
 });
+
+router.get("/api/homes", homes.getAllHomes);
 
 router.get('/homes/seed', (req, res)=>{
   Home.create(homeSeed, (err, data)=>{
